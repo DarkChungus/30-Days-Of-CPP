ad795ba531b9de53452e922d5e98186eeb4b851e
godchuungus <96650932+DarkChungus@users.noreply.github.com>
2024-12-16 20:57:16 +0545

    Create Day 12 and 13(important note).cpp

---
+++ Day 12 and 13(important note).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello!" << "\n";
+	cout << "It looks like I am slacking off in my GitHub." << "\n";
+	cout << "I am sorry for this. I have a lot of things going on, I have a new tuition teacher who only wants me to get full marks, ";
+	cout << "I am basically juggling a hell lot of balls, and keeping a keen eye on each one of them." << "\n";
+	cout << "Hopefully I can continue to learn C++ even under these harsh conditions." << "\n";
+	cout << "See you all next program! o7";
+
+	return 0;
+}
