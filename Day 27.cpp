70cac44541307483e8dacb599d593bbc63afd686
Prakrit Gajurel <96650932+DarkChungus@users.noreply.github.com>
2025-01-20 15:37:30 +0545

    Create Day 27.cpp

---
+++ Day 27.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+	long long num, sum = 0;
+	cin >> num;
+	for(int i=1; i<=(num-1); i++){
+		long long temp;
+		cin >> temp;
+		sum+=temp;
+	}
+	long long ans;
+	ans = num*(num+1)/2 - sum;
+	cout << ans << endl;
+}
+
+// Apparently it's the second best way to solve this. Am proud of it
